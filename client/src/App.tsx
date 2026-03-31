ea2828377b466ad324e56a183633422e69e4ccd0
wascar0410 <45834879+wascar0410@users.noreply.github.com>
2026-03-31 13:08:08 -0400

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import DashboardLayout from "./components/DashboardLayout";
 import About from "./pages/About";
 import Dashboard from "./pages/Dashboard";
 import Loads from "./pages/Loads";
+import Finance from "./pages/Finance";
 
 function DashboardPage() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
       <Route path="/about" component={About} />
       <Route path="/dashboard" component={DashboardPage} />
       <Route path="/loads" component={Loads} />
+      <Route path="/finance" component={Finance} />
     </Switch>
   );
 }
