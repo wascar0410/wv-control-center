71da8512843caf29a80e8e90b23cac164899e99a
wascar0410 <45834879+wascar0410@users.noreply.github.com>
2026-03-31 12:40:16 -0400

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Switch } from "wouter";
 import DashboardLayout from "./components/DashboardLayout";
 import About from "./pages/About";
 import Dashboard from "./pages/Dashboard";
+import Loads from "./pages/Loads";
 
 function DashboardPage() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
       <Route path="/" component={About} />
       <Route path="/about" component={About} />
       <Route path="/dashboard" component={DashboardPage} />
+      <Route path="/loads" component={Loads} />
     </Switch>
   );
 }
