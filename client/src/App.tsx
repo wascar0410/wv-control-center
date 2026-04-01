0eb883737cc2030e0ed90c3ad799c52ed9fcda95
wascar0410 <45834879+wascar0410@users.noreply.github.com>
2026-04-01 11:04:17 -0400

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,117 +1,18 @@
 import { Route, Switch } from "wouter";
-import DashboardLayout from "./components/DashboardLayout";
-import About from "./pages/About";
-import Dashboard from "./pages/Dashboard";
-import Loads from "./pages/Loads";
-import Finance from "./pages/Finance";
-import Transactions from "./pages/Transactions";
-import Partnership from "./pages/Partnership";
-import UserProfile from "./pages/UserProfile";
-import DriverViewProduction from "./pages/DriverViewProduction";
-import ExecutiveDashboard from "./pages/ExecutiveDashboard";
-import Quotation from "./pages/Quotation";
-import QuotationHistory from "./pages/QuotationHistory";
-import ImportBrokerLoads from "./pages/ImportBrokerLoads";
-import BrokerLoadsManagement from "./pages/BrokerLoadsManagement";
-import AccountingFinance from "./pages/AccountingFinance";
 
-
-function DashboardPage() {
+function HomePage() {
   return (
-    <DashboardLayout>
-      <Dashboard />
-    </DashboardLayout>
-  );
-}
-
-function LoadsPage() {
-  return (
-    <DashboardLayout>
-      <Loads />
-    </DashboardLayout>
-  );
-}
-
-function FinancePage() {
-  return (
-    <DashboardLayout>
-      <Finance />
-    </DashboardLayout>
-  );
-}
-
-function TransactionsPage() {
-  return (
-    <DashboardLayout>
-      <Transactions />
-    </DashboardLayout>
-  );
-}
-
-function PartnershipPage() {
-  return (
-    <DashboardLayout>
-      <Partnership />
-    </DashboardLayout>
-  );
-}
-
-function ProfilePage() {
-  return (
-    <DashboardLayout>
-      <UserProfile />
-    </DashboardLayout>
-  );
-}
-
-function ExecutiveDashboardPage() {
-  return (
-    <DashboardLayout>
-      <ExecutiveDashboard />
-    </DashboardLayout>
-  );
-}
-
-function QuotationPage() {
-  return <Quotation />;
-}
-
-function QuotationHistoryPage() {
-  return (
-    <DashboardLayout>
-      <QuotationHistory />
-    </DashboardLayout>
-  );
-}
-
-function ImportBrokerLoadsPage() {
-  return (
-    <DashboardLayout>
-      <ImportBrokerLoads />
-    </DashboardLayout>
-  );
-}
-
-function BrokerLoadsManagementPage() {
-  return (
-    <DashboardLayout>
-      <BrokerLoadsManagement />
-    </DashboardLayout>
-  );
-}
-
-function AccountingFinancePage() {
-  return (
-    <DashboardLayout>
-      <AccountingFinance />
-    </DashboardLayout>
+    <div style={{ padding: 24 }}>
+      <h1>HOME TEST 999</h1>
+      <p>Si ves esto, App.tsx nuevo sí está cargando.</p>
+    </div>
   );
 }
 
 function BusinessSettingsPage() {
   return (
     <div style={{ padding: 24 }}>
-      <h1>BUSINESS SETTINGS INLINE TEST 777</h1>
+      <h1>BUSINESS SETTINGS INLINE TEST 999</h1>
       <p>Si ves esto, la ruta funciona.</p>
     </div>
   );
@@ -120,21 +21,7 @@ function BusinessSettingsPage() {
 export default function App() {
   return (
     <Switch>
-      <Route path="/" component={About} />
-      <Route path="/about" component={About} />
-      <Route path="/dashboard" component={DashboardPage} />
-      <Route path="/loads" component={LoadsPage} />
-      <Route path="/finance" component={FinancePage} />
-      <Route path="/transactions" component={TransactionsPage} />
-      <Route path="/partnership" component={PartnershipPage} />
-      <Route path="/profile" component={ProfilePage} />
-      <Route path="/driver" component={DriverViewProduction} />
-      <Route path="/executive-dashboard" component={ExecutiveDashboardPage} />
-      <Route path="/quotation" component={QuotationPage} />
-      <Route path="/quotation-history" component={QuotationHistoryPage} />
-      <Route path="/import-broker-loads" component={ImportBrokerLoadsPage} />
-      <Route path="/broker-loads-management" component={BrokerLoadsManagementPage} />
-      <Route path="/accounting-finance" component={AccountingFinancePage} />
+      <Route path="/" component={HomePage} />
       <Route path="/business-settings" component={BusinessSettingsPage} />
     </Switch>
   );
